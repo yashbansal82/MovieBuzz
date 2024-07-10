f4e83375a0bfc47b12abdbc3f35a13b96ab7e2a6
yashbansal82 <bansalyash569@gmail.com>
2024-07-11 01:22:26 +0530

    image removed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -37,7 +37,7 @@ function Header() {
         toggle ? "from-black mt-0 pt-3" : " from-black mt-0 pt-3"
       }`}
     >
-      <img className="w-56" src="https://th.bing.com/th/id/R.715e69176d3cec74504b3ae00f2676c6?rik=vGDJl%2bfFL64qmw&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f4%2fNetflix-Logo-HD.png&ehk=aWIF5J5TR6tD4IrfNFhXE0dNVq%2f6n%2f6OViRGEsRS%2bNg%3d&risl=&pid=ImgRaw&r=0" alt="logo" />
+      {/* <img className="w-56" src="https://th.bing.com/th/id/R.715e69176d3cec74504b3ae00f2676c6?rik=vGDJl%2bfFL64qmw&riu=http%3a%2f%2fwww.pngall.com%2fwp-content%2fuploads%2f4%2fNetflix-Logo-HD.png&ehk=aWIF5J5TR6tD4IrfNFhXE0dNVq%2f6n%2f6OViRGEsRS%2bNg%3d&risl=&pid=ImgRaw&r=0" alt="logo" /> */}
       {user && (
         <div className="flex items-center">
           <IoIosArrowDropdown size="24px" color="white" />
